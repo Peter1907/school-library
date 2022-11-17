c54acef9da06d3c1f3f5f9f4586eb3501955d2b1
Peter1907 <peter.nady.1408@gmail.com>
2022-11-17 06:22:18 +0200

    Add associations between classes

---
+++ classroom.rb
@@ -0,0 +1,13 @@
+class classroom
+  attr_accessor :label
+
+  def initialize(label)
+    @label = label
+    @students = []
+  end
+
+  def add_student(student)
+    @students << student
+    student.classroom = self
+  end
+end
--- person.rb
+++ person.rb
@@ -3,17 +3,17 @@ require './capitalize_decorator'
 require './trimmer_decorator'
 
 class Person < Nameable
+  attr_accessor :name, :age
+  attr_reader :id
+
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
-
-  attr_accessor :name, :age
-
-  attr_reader :id
 
   def correct_name
     @name
@@ -21,6 +21,11 @@ class Person < Nameable
 
   def can_use_services?
     of_age?
+  end
+
+  def add_rental(rental)
+    @rentals << rental
+    rental.person = self
   end
 
   private
